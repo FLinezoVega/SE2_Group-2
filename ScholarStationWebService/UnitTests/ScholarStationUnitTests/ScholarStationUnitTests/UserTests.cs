7218cd61523e7170713ac462bfaa03a942e620a6
NINJAchop957 <ninjachop957@gmail.com>
2018-03-23 21:37:04 -0500

    Added more to UserTests: tested isNull() method in User class (21:36, 23 March 2018)

--- ScholarStationWebService/UnitTests/ScholarStationUnitTests/ScholarStationUnitTests/UserTests.cs
+++ ScholarStationWebService/UnitTests/ScholarStationUnitTests/ScholarStationUnitTests/UserTests.cs
@@ -13,14 +13,25 @@ namespace ScholarStationUnitTests
         {
             //Arrange
             var userB = new User();
+            userB.UserID = "Bub Skebulba";
+            userB.University = "Clown College";
+            userB.Bio = "One time I ate sand in math class.";
             var sut = userB;
+
+            var userA = new User();
+            userA.UserID = null;
+            userA.University = null;
+            userA.Bio = null;
+            var sut1 = userA;
 
 
             //Act
             sut.isNull();
+            sut1.isNull();
 
             //Assert
-            Assert.AreNotEqual(sut.isNull(), null);
+            Assert.AreNotEqual(sut.isNull(), true);
+            Assert.AreEqual(sut1.isNull(), true);
 
         }
     }
