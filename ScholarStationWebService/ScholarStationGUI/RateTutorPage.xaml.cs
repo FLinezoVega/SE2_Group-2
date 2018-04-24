4bd52354d63f20b18bf1c9cdab5229c70e3038af
B-pointer <bkp5@students.uwf.edu>
2018-04-23 19:24:24 -0500

    adding the tutor rating page. still need to write dataaccess for it though

---
+++ ScholarStationWebService/ScholarStationGUI/RateTutorPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using DataAccessInterfaces;
+
+
+namespace ScholarStationGUI
+{
+    /// <summary>
+    /// Interaction logic for RateTutorPage.xaml
+    /// </summary>
+
+
+    public partial class RateTutorPage : Page
+    {
+        IDataManager manager;
+        public RateTutorPage(IDataManager manager)
+        {
+            this.manager = manager;
+            InitializeComponent();
+        }
+    }
+}
