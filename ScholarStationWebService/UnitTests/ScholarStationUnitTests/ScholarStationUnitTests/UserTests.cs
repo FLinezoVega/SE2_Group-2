fb47cd8468706f93b717282c5541c437eb3c068b
pl20 <31527292+pl20@users.noreply.github.com>
2018-04-27 11:11:14 -0500

    Updated testing for User Class

--- ScholarStationWebService/UnitTests/ScholarStationUnitTests/ScholarStationUnitTests/UserTests.cs
+++ ScholarStationWebService/UnitTests/ScholarStationUnitTests/ScholarStationUnitTests/UserTests.cs
@@ -16,14 +16,15 @@ namespace ScholarStationUnitTests
             userB.UserID = "Bub Skebulba";
             userB.University = "Clown College";
             userB.Bio = "One time I ate sand in math class.";
+            userB.Email = "sandy@students.uwf.edu";
             var sut = userB;
 
             var userA = new User();
             userA.UserID = null;
             userA.University = null;
             userA.Bio = null;
+            userA.Email = null;
             var sut1 = userA;
-
 
             //Act
             var sutResult = sut.isNull();
