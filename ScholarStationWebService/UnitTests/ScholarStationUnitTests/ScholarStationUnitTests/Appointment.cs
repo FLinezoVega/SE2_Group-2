f38be4562790ce3b246a11fc3516a3710246c743
pl20 <31527292+pl20@users.noreply.github.com>
2018-04-28 19:10:10 -0500

    Finished AppointmentTest Class

--- ScholarStationWebService/UnitTests/ScholarStationUnitTests/ScholarStationUnitTests/Appointment.cs
+++ ScholarStationWebService/UnitTests/ScholarStationUnitTests/ScholarStationUnitTests/Appointment.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Runtime.Serialization;
 
 namespace DataClasses
 {
@@ -12,28 +13,41 @@ namespace DataClasses
         private string clientID;
         private string timeslot;
         private int id;
+        private string clientMail;
 
+        [DataMember]
         public string TutorID
         {
-            get;
-            set;
+            get { return this.tutorID; }
+            set { this.tutorID = value; }
         }
 
+        [DataMember]
+        public string ClientMail
+        {
+            get { return this.clientMail; }
+            set { this.clientMail = value; }
+        }
+
+        [DataMember]
         public string ClientID
         {
-            get;
-            set;
+            get { return this.clientID; }
+            set { this.clientID = value; }
         }
 
+        [DataMember]
         public string Timeslot
         {
-            get;
-            set;
+            get { return this.timeslot; }
+            set { this.timeslot = value; }
         }
 
+        [DataMember]
         public int ID
         {
             get { return this.id; }
+            set { this.id = value; }
         }
     }
 }
